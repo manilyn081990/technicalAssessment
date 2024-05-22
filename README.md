ac50bf6fea0f96b37fe08ec55eb8a15eb40f74f6
J Franco Ray <5762748+formidablefrank@users.noreply.github.com>
2024-05-23 00:59:35 +0800

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# inventory_mgmt
+# Simple inventory management system in C# .NET 8.0
+Technical skills assessment for QA specialist
---
+++ inventory_mgmt/Service/OperationManager.cs
@@ -0,0 +1,67 @@
+﻿using InventoryMgmt.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMgmt.Service
+{
+    public class OperationManager: IOperationManager
+    {
+        private IInventoryManager _inventoryManager = new InventoryManager();
+        public void StartOperation(int operationId)
+        {
+            switch (operationId)
+            {
+                case 1:
+                    AddOperation();
+                    break;
+                case 2:
+                    RemoveOperation();
+                    break;
+                case 3:
+                    UpdateOperation();
+                    break;
+                case 4:
+                    _inventoryManager.GetTotalValue();
+                    break;
+                case 5:
+                    _inventoryManager.ListProducts();
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid operation! Please try again.");
+            }
+        }
+
+        private void AddOperation()
+        {
+            Console.WriteLine("Add a product");
+            Console.WriteLine("Name:");
+            var productName = Console.ReadLine();
+            Console.WriteLine("Quantity:");
+            var quantity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price:");
+            var price = Convert.ToDecimal(Console.ReadLine());
+            _inventoryManager.AddNewProduct(productName, quantity, price);
+        }
+
+        private void RemoveOperation()
+        {
+            Console.WriteLine("Remove a product");
+            Console.WriteLine("Product ID:");
+            var productId = Convert.ToInt32(Console.ReadLine());
+            _inventoryManager.RemoveProduct(productId);
+        }
+
+        private void UpdateOperation()
+        {
+            Console.WriteLine("Update a product");
+            Console.WriteLine("Product ID:");
+            var productId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("New quantity:");
+            var quantity = Convert.ToInt32(Console.ReadLine());
+            _inventoryManager.UpdateProduct(productId, quantity);
+        }
+    }
+}
